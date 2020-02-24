28642c5b756dd6e9adb2f89901fb74d260b4a507
yamat0jp <terukohietori@gmail.com>
2020-02-24 22:42:26 +0900

    ごめんなさい。古いファイルがまだ残っていました。

--- mini.dpr
+++ mini.dpr
@@ -1,28 +1,12 @@
 program mini;
 
-uses
-  Vcl.SvcMgr,
-  Unit1 in 'Unit1.pas' {Service1: TService};
+{$R *.res}
 
-{$R *.RES}
+uses Windows;
 
 begin
-  // Windows 2003 Server では、CoRegisterClassObject の前に StartServiceCtrlDispatcher を
-  // 呼び出す必要があります。前者は Application.Initialize で間接的に
-  // 呼び出されることがあります。TServiceApplication.DelayInitialize では、
-  // (StartServiceCtrlDispatcher が呼び出された後で) TService.Main から
-  // Application.Initialize を呼び出すことができます。
-  //
-  // Application オブジェクトの遅延初期化は、初期化より前に発生する
-  // イベント (たとえば TService.OnCreate など) に影響を及ぼす
-  // 可能性があります。これを推奨するのは、ServiceApplication が、
-  // Windows 2003 Server で使用するためのもので、かつ OLE に
-  // クラス オブジェクトを登録する場合だけです。
-  //
-  // Application.DelayInitialize := True;
-  //
-  if not Application.DelayInitialize or Application.Installing then
-    Application.Initialize;
-  Application.CreateForm(TService1, Service1);
-  Application.Run;
+  Keybd_event(VK_LWIN,0,0,0);
+  Keybd_event(Ord('M'),0,0,0);
+  Keybd_event(Ord('M'),0,KEYEVENTF_KEYUP,0);
+  Keybd_event(VK_LWIN,0,KEYEVENTF_KEYUP,0);
 end.
